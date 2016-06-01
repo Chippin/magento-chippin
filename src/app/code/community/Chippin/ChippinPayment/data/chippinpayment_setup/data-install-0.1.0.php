8588fa1c9f00df07375c591733e9b3774e82d655
Alistair Stead <alistair_stead@me.com>
2016-06-01 22:23:42 +0100

    Order statuses are now correctly created at install

---
+++ src/app/code/community/Chippin/ChippinPayment/data/chippinpayment_setup/data-install-0.1.0.php
@@ -0,0 +1,67 @@
+<?php
+$installer = $this;
+$connection = $installer->getConnection();
+$installer->startSetup();
+
+Mage::log('Installing Chippin oder statuses');
+
+$data = array(
+    array('status' => 'chippin_pending_payment', 'label' => 'Chippin Pending'),
+    array('status' => 'chippin_pending_completion', 'label' => 'Chippin Incomplete'),
+    array('status' => 'chippin_complete', 'label' => 'Chippin Complete'),
+    array('status' => 'chippin_rejected', 'label' => 'Chippin Rejected'),
+    array('status' => 'chippin_timedout', 'label' => 'Chippin Timed Out'),
+    array('status' => 'chippin_canceled', 'label' => 'Chippin Canceled')
+);
+
+$mappingData = array(
+    array(
+        'status' => 'chippin_pending_payment',
+        'state' => 'pending_payment',
+        'is_default' => 0
+    ),
+    array(
+        'status' => 'chippin_pending_completion',
+        'state' => 'pending_payment',
+        'is_default' => 0
+    ),
+    array(
+        'status' => 'chippin_complete',
+        'state' => 'processing',
+        'is_default' => 0
+    ),
+    array(
+        'status' => 'chippin_rejected',
+        'state' => 'holded',
+        'is_default' => 0
+    ),
+    array(
+        'status' => 'chippin_timedout',
+        'state' => 'holded',
+        'is_default' => 0
+    ),
+    array(
+        'status' => 'chippin_canceled',
+        'state' => 'canceled',
+        'is_default' => 0
+    )
+);
+
+$statusTable = $installer->getTable('sales/order_status');
+$statusStateTable = $installer->getTable('sales/order_status_state');
+
+// Insert statuses
+$installer->getConnection()->insertArray(
+    $statusTable,
+    array('status', 'label'),
+    $data
+);
+
+// Insert states and mapping of statuses to states
+$installer->getConnection()->insertArray(
+    $statusStateTable,
+    array('status', 'state', 'is_default'),
+    $mappingData
+);
+
+$installer->endSetup();
